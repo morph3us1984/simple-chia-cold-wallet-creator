6f78f2f11e1b552f9110032a65082d7037abf6b1
morph3us1984 <89521988+morph3us1984@users.noreply.github.com>
2022-04-30 03:58:55 +0200

    Update cold-wallet.sh

--- cold-wallet.sh
+++ cold-wallet.sh
@@ -1,9 +1,11 @@
 #/bin/bash
-sudo apt update && \ 
-sudo apt upgrade -y && \
-sudo apt install git htop nano ssh && \ 
+#sudo apt update && \ 
+#sudo apt upgrade -y && \
+sudo apt install wget && \ 
+wget https://github.com/Chia-Network/chia-blockchain/releases/download/1.3.4/chia-blockchain_1.3.4_amd64.deb && \ 
+sudo dpkg -i chia-blockchain_1.3.4_amd64.deb && \ 
 cd ~ && \
-git clone https://github.com/Chia-Network/chia-blockchain && \
+#git clone https://github.com/Chia-Network/chia-blockchain && \
 cd chia-blockchain && \
 sh install.sh && \
 . ./activate && \
