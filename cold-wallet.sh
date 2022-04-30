a15644ddfa6384a7b903c57c708e894ea7208848
morph3us1984 <89521988+morph3us1984@users.noreply.github.com>
2022-04-30 04:05:59 +0200

    Update cold-wallet.sh

--- cold-wallet.sh
+++ cold-wallet.sh
@@ -1,17 +1,8 @@
 #/bin/bash
-#sudo apt update && \ 
-#sudo apt upgrade -y && \
-#sudo apt install wget && \ 
 wget https://github.com/Chia-Network/chia-blockchain/releases/download/1.3.4/chia-blockchain_1.3.4_amd64.deb && \ 
 sudo dpkg -i chia-blockchain_1.3.4_amd64.deb && \ 
-cd ~ && \
-#git clone https://github.com/Chia-Network/chia-blockchain && \
-cd chia-blockchain && \
-sh install.sh && \
-. ./activate && \
+cd /usr/lib/chia-blockchain/resources/app.asar.unpacked/daemon && \
 chia init && \
 chia init --fix-ssl-permissions ; \
 chia keys generate && \
 chia keys show --show-mnemonic-seed && \ 
-deactivate && \
-cd ~
