1739206bd6f9dc98ce10830a72b05d2e0eeda453
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-05-24 20:36:50 +0200

    Update cold-wallet.sh

--- cold-wallet.sh
+++ cold-wallet.sh
@@ -5,12 +5,7 @@ cd /usr/lib/chia-blockchain/resources/app.asar.unpacked/daemon && \
 ./chia init && \
 ./chia init --fix-ssl-permissions ; \
 ./chia keys generate && \
-./chia keys show --show-mnemonic-seed
-
-# qrencode generates and displays a QR-code of the wallet address within the CLI 
-
-sudo apt-get install qrencode -y && 
-sudo apt --fix-broken install -y &&
-wallet_address=$(./chia keys show --show-mnemonic-seed | grep 't\?xch.' | cut -c 23-) | echo "$(tput setaf 2) $wallet_address" &&
-qrencode $wallet_address -t ANSI256
+./chia keys show --show-mnemonic-seed && \ 
+sudo apt-get install qrencode -y && \
+wallet_address=$(./chia keys show --show-mnemonic-seed | grep 't\?xch.' | cut -c 23-) | echo "$(tput setaf 2) $wallet_address" ; qrencode $wallet_address -t ANSI256
 
