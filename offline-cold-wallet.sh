9392659bb21cb3755b45e1fcc5c41657a71d050b
morph3us1984 <89521988+morph3us1984@users.noreply.github.com>
2022-06-27 00:47:30 +0200

    Update offline-cold-wallet.sh
    changed to use "chia keys generate_and_print" so keys are NOT added to the keyring. Adding an extra layer of security.

--- offline-cold-wallet.sh
+++ offline-cold-wallet.sh
@@ -3,5 +3,4 @@ sudo dpkg -i chia-blockchain_*.deb && \
 cd /usr/lib/chia-blockchain/resources/app.asar.unpacked/daemon && \
 ./chia init && \
 ./chia init --fix-ssl-permissions ; \
-./chia keys generate && \
-./chia keys show --show-mnemonic-seed
+./chia keys generate_and_print
